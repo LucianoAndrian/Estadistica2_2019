1d22bb7914d939e6ad15bc26d355f80f3bed50a8
V <44793625+LucianoAndrian@users.noreply.github.com>
2019-07-10 19:23:57 -0300

    Add files via upload

--- P7/Practica7.R
+++ P7/Practica7.R
@@ -33,28 +33,80 @@ datos<-read.table("datos_SOI.txt")
 datos<-datos[,-1]
 datos<-t(datos)
 datos2<-array(datos,dim=c(12*69,1))
-
-#para usar analize.wavelet
-dat<-data.frame(seq(1:length(datos2)),datos2)
-
 plot.ts(datos2)
 
-#todas las funciones
+# la serie presenta gran variabilida
+
+# lo de simpre, tiene tendencia, saltos?
+
+# el periodo de la serie va desde 1950 a 2018
+
+# sabemos de un salto climatico en algunas variables a partir de anio '79 '80
+# busco un salto entre esas fechas con el test de yamamoto
+Y<-vector()
+for(i in 1:48){
+  datosA<-datos2[1:(336+i)]
+  datosB<-datos2[(337+i):828]
+  mediaA<-mean(datosA)
+  mediaB<-mean(datosB)
+  cA<-sd(datosA)*qt(0.975,df=827)*1/sqrt(828-1)
+  cB<-sd(datosB)*qt(0.975,df=827)*1/sqrt(828-1)
+  #tcritico 1,96
+  Y[i]<-(mediaA-mediaB)/(cA+cB)
+  rm(datosB)
+}
+#un salto a partir de la posicion 337+1--->correspondiente al año 1978
+#creo la serie nueva
+
+nueva<-c(datosA+mediaB,datosB)
+
+# la serie original tiene tendencia?
+summary(lm(datos2~seq(1:828)))
+# no presenta tendencia significativa ( pero casi )
+
+#la serie sin el salto 
+summary(lm(nueva~seq(1:828)))
+# tamoco y el p valor es mucho grande que el alfa
+
+# voy a seguir trabajndo utilizando la serie a la que le quite el salto, ya que si bien 
+# ninguna de las dos presenta tendencia significativa, la 2da tiene un pendiente menor
+
+
+# antes de wavelet me intereza aplicar analisis armonico y espectral 
+
 source("C:/Users/Alumno/Desktop/Estadistica2/Scripts/FUNCIONES.R")
+# delta t 838 meses 
+# este indice se usa como monitoreo para el ninio
+# el cual presenta una periodicidad, con una varaibiidad un poco alta, de 3 años
+# por lo tanto esperarimos ver armonicos relacionads con esa frecuencia, que serian ~23
+# y periodos significativos cercanos a esa frecuencia
+funcion_armonico(nueva) # no se ve nada... muy poca varianza explicada debido a la enorme cantidad de armonicos
+                        # los menores armonicos ( 5 - 31 ) son los explican mas
+# el armonico 19 es el que mas varianza explica~5.56%
+# periodo asociado 828/19--->3.63 anios!! bieen
 
-funcion_armonico(dat[,2])
+espectro(as.matrix(nueva))
+#[1] 165.600000 124.200000  62.100000  >>>55.200000<<<  >>>45.163636<<<  41.400000  31.050000 
+#29.223529   4.072131
+#[10]   3.268421   3.205161   2.871676   2.855172   2.560825   2.547692   2.534694
+#2.496482   2.484000
+#[19]   2.471642   2.459406   2.400000   2.388462   2.247964   2.237838   2.227803
+#2.198230   2.188546
+#[28]   2.036066   2.027755   2.003226
 
-espectro(datos2)
+# se ven un monton de periodos significativos, los relacionados al ninio serian los 45 y 55 meses
+
+dat<-data.frame(seq(1:length(nueva)),nueva)
 
 library(WaveletComp)
 
 my.wt =analyze.wavelet(dat, my.series=2)
 
-png('waveletfigura_1.png',width = 800,height = 600,units = "px")
+
 wt.image(my.wt,color.key="q", legend.params=list(lab="wavelet power levels"),
          siglvl = 0.05,timelab='Tiempo',periodlab = 'Periodo',main = 'Wavelet nro.2',
          plot.ridge = F)
-dev.off()
+
 
 #se ve mejor con cuantiles que en intervalos 
 
@@ -71,5 +123,7 @@ prueba2<-monthyear[q]
 
 wt.image(my.wt, main = "wavelet power spectrum", 
          periodlab = "period (months)", timelab = "month and year",
-         spec.time.axis = list(at = q, labels = prueba2),color.key='q')
-###
+         spec.time.axis = list(at = q, labels = prueba2),color.key='q',plot.ridge = F)
+
+# se puede observar periodos significativos dentro del rango que se espera encontrar relacionados con 
+# el ninio
